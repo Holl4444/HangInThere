ea4145304da4bf004cf029814a2380621e528691
Holly <162161116+Holl4444@users.noreply.github.com>
2025-02-08 15:52:36 +0000

    Create component framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function Hangman() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <section className="gameBoard">
+        <header>
+          <h1>Hang In There</h1>
+          <p>Guess the word in under 8 attempts to keep the kitties safe.</p>
+        </header>
+        <article className="msg-container"></article>
+        <div className="cat-basket">
+          [cats]
+        </div>
+        <div className="word-div">
+          [letters]
+        </div>
+        <div className="alphabet">
+          [letters]
+        </div>
+        <button className="reset">New Game</button>
+      </section>
+    </main>
+  );
 }
 
-export default App
+/**
+ * Project planning:
+ *
+ * Questions to ask yourself before writing any code:
+ *
+ * - What are the main containers of elements I need
+ *   in this app?
+ * Board
+ *  - Intro
+ *      - Title
+ *      - Instruction
+ *  - Message container
+ *      - Message
+ *  - Victim Container
+ *      - Victims
+ *  - Word
+ *      - Letters
+ *  - Alphabet
+ *      - Letters
+ *  - Reset
+ *
+ *
+ * - What values will need to be saved in state vs.
+ *   what values can be derived from the state?
+ *
+ *      State of the word
+ *      State of the victims
+ *
+ * - How will the user interact with the app? What
+ *   events do I need to handle?
+ *
+ *      New Game Button
+ *      Letter Choice
+ *      Victim status indicator
+ *      Letter checked status indicator
+ *      Win / Loss Message
+ *
+ */
