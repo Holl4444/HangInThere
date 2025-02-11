9f4ee87ffea61a4ef965afc554eafc46f0ab1635
Holl4444 <holly.cello@gmail.com>
2025-02-11 17:17:04 +0000

    sentry missed file

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,8 @@ import App from './App.jsx';
 import * as Sentry from '@sentry/react';
 
 Sentry.init({
-  dsn: 'https://3aba6294c87272d7e47801db8abbcdb9@o4508800319160320.ingest.de.sentry.io/4508801682047056',
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: [
-    'localhost',
-    /^https:\/\/yourserver\.io\/api/,
-  ],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  dsn: 'https://4aa2e32806280bc0eb338d38117b1f4f@o4508800319160320.ingest.de.sentry.io/4508801465647184',
+  integrations: [],
 });
 
 createRoot(document.getElementById('root')).render(
