22fc458e7066bca39d405c4bb6c0598101ea2801
Holl4444 <holly.cello@gmail.com>
2025-02-11 21:11:12 +0000

    remove sentry button

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,21 @@ import App from './App.jsx';
 import * as Sentry from '@sentry/react';
 
 Sentry.init({
-  dsn: 'https://4aa2e32806280bc0eb338d38117b1f4f@o4508800319160320.ingest.de.sentry.io/4508801465647184',
-  integrations: [],
+  dsn: 'https://e8ba4e88268083bc735f8da372cc5fba@o4508800319160320.ingest.de.sentry.io/4508801752694864',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    'localhost',
+    /^https:\/\/yourserver\.io\/api/,
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 createRoot(document.getElementById('root')).render(
