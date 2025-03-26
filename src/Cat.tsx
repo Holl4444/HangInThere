660ecd7dd334656df01caa6fbd8bb55c707c9045
Holl4444 <holly.cello@gmail.com>
2025-03-26 20:56:33 +0000

    Add: cat names appear on hover

--- src/Cat.tsx
+++ src/Cat.tsx
@@ -1,5 +1,4 @@
 import { CatProps } from './Cats';
-import styles from './Cat.module.css';
 
 export default function Cat({
   backgroundColor,
@@ -13,18 +12,14 @@ export default function Cat({
     color: color,
   };
 
-   return (
-    <button
-      className={className}
-      name={name}
-      style={inlineStyles}
-    >
+  return (
+    <button className={className} name={name} style={inlineStyles}>
       <img
         src={`${srcName}.png`}
         alt={`A cat named ${name}.`}
         className={className}
       />
-       <p className='cat-name'>{name}</p>
+      <p className="cat-name">{name}</p>
     </button>
   );
 }
