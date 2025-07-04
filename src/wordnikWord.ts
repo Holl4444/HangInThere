d9a16e9be4ec21a00a6080a750ac70d8e5a7ecac
Holl4444 <holly.cello@gmail.com>
2025-07-04 17:38:18 +0100

    fix: roman numerals!

--- src/wordnikWord.ts
+++ src/wordnikWord.ts
@@ -11,9 +11,10 @@ let wordnikCache: WordnikResponse[] = []; // Don't need state as this is a modul
 
 //MinCorpusCount controls how common the words are. Higher is more common.
 
-const wordnikUrl = `https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minLength=${MIN_WORD_LENGTH}&maxLength=${MAX_WORD_LENGTH}&limit=10&minCorpusCount=2000&api_key=${
+const wordnikUrl = `https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minLength=${MIN_WORD_LENGTH}&maxLength=${MAX_WORD_LENGTH}&limit=10&minCorpusCount=5000&excludePartOfSpeech=given-name,family-name,proper-noun&api_key=${
   import.meta.env.VITE_WORDNIK_API_KEY
 }`;
+
 
 export default async function fetchWordnik(): Promise<string | null> {
   try {
@@ -25,17 +26,20 @@ export default async function fetchWordnik(): Promise<string | null> {
         ...wordnikCache.slice(0, randomIndex),
         ...wordnikCache.slice(randomIndex + 1),
       ];
-      return wordnikWord.toUpperCase();
+      return wordnikWord;
     }
 
     //fetch from api;
     const response = await fetch(wordnikUrl);
     const wordnikArray: WordnikResponse[] = await response.json();
     if (!wordnikArray || wordnikArray.length === 0) return null;
-    const regex = /^[a-zA-Z]+$/; // No hyphens or spaces
-    const cleanWordnikArray: WordnikResponse[] = wordnikArray.filter(
-      (word) => regex.test(word.word)
-    );
+    const regex = /^[a-zA-Z]+$/;
+    const romanNumeralRegex = /^[IVXLCDM]+$/; // Unbelievably needed :)
+
+    const cleanWordnikArray: WordnikResponse[] = wordnikArray
+      .map(word => ({ ...word, word: word.word.toUpperCase() }))
+      .filter(word => regex.test(word.word) &&!romanNumeralRegex.test(word.word));
+    
     if (cleanWordnikArray.length > 1) {
       console.log(cleanWordnikArray);
       wordnikCache = cleanWordnikArray.slice(1); // stash remaining words
