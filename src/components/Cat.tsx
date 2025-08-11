a59f8c858ed1d40824a5a705708672a640fad2e9
Holl4444 <holly.cello@gmail.com>
2025-08-11 14:59:33 +0100

    add: Test button & .webp images
    - modify src to use .png as fallback

--- src/components/Cat.tsx
+++ src/components/Cat.tsx
@@ -14,11 +14,15 @@ export default function Cat({
 
   return (
     <button className={className} name={name} style={inlineStyles}>
-      <img
-        src={`${srcName}.png`}
-        alt={`A cat named ${name}.`}
-        className={className}
-      />
+      {/* Using the picture tag to specify more than one image source. The browser will then pick the first supported image source or fallback to the img tag */}
+      <picture>
+        <source srcSet={`${srcName}.webp`} type="image/webp" />
+        <img
+          src={`${srcName}.png`}
+          alt={`A cat named ${name}.`}
+          className={className}
+        />
+      </picture>
       <p className="cat-name">{name}</p>
     </button>
   );
